60d361a828e87c94b3e40f2eb19c170ade9598d6
Khmelevskiy Ilya <khmelevskii.ilia@gmail.com>
2021-03-19 13:31:56 +0100

    Was added layout page layout component

---
+++ frontend/components/Page.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export default function Page({children, cool}) {
+    return (
+        <div>
+            <h2>I am the page component</h2>
+            <h3>{cool}</h3>
+            {children}
+        </div>
+    );
+}
+
+Page.protoTypes = {
+    cool: PropTypes.string,
+    children: PropTypes.oneOf([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+};
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,5 +1,7 @@
 export default function SellPage() {
-    return <div>
-        <p>Hello!</p>
-    </div>
+    return (
+        <>
+            <p>Hello i am the sell page!</p>
+        </>
+    );
 }
